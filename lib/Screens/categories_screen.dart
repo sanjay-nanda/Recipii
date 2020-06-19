25cb6c09b61583c0f1ae2f0db376193c41ddd473
Sanjay <sanrahnan@gmail.com>
2020-06-19 11:35:01 +0530

    initial commit

---
+++ lib/Screens/categories_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../Widgets/category_item.dart';
+
+import '../dummy.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+          padding: EdgeInsets.all(20),
+          children: DUMMY_CATEGORIES
+              .map((e) => CategoryItem(e.color, e.title, e.id))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          )),
+    );
+  }
+}
---
+++ lib/Screens/category_meals_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../Widgets/meal_item.dart';
+import '../dummy.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  /*  final String id;
+  final String name;
+
+  CategoryMealsScreen(this.id, this.name); */
+
+  static const routeName = "category-meals";
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final String name = routeArgs['name'];
+    final String Id = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where(
+      (element) {
+        return element.categories.contains(Id);
+      },
+    ).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability,
+              imageUrl: categoryMeals[index].imageUrl);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
